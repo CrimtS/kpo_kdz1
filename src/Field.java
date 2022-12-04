427196d824a1b9db3af50f4afd928a5ac005f509
fedor <daldalron007@gmail.com>
2022-12-04 20:26:54 +0300

    draw done

--- src/Field.java
+++ src/Field.java
@@ -1,3 +1,18 @@
 public class Field {
-    void draw(){}
+    int[][] data = new int[8][8];
+    char[] dot_variants = {' ', '○', '●'};
+    String horiz_border = "+-----+-----+-----+-----+-----+-----+-----+-----+";
+
+    void draw() {
+        System.out.println(horiz_border);
+        for (int i = 0; i < 8; i++) {
+            StringBuilder line_of_dots = new StringBuilder();
+            for (int num : data[i]) {
+                line_of_dots.append("|  ").append(dot_variants[num]).append("  ");
+            }
+            System.out.println(line_of_dots.append("|"));
+            System.out.println(horiz_border);
+        }
+    }
+
 }
